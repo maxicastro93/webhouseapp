9cdce2fcb7dbd3121a33b9c97705b008a2d1fdcb
Maximiliano Castro <maaxi.castro@gmail.com>
2021-04-23 19:56:17 -0300

    CAMBIOS EN LA CONFIG DB

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,11 @@
 module.exports = 
 {
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database:'database_inmobiliaria'
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'b9c684a47d67a8',
+        password: '78e2cc85',
+        database:'heroku_2d0ebf5c733ab7a'
     }
 
 };
+//mysql://:@/?reconnect=true
